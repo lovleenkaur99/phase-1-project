98604bfcc8c37cd611712290469797e53090ad5d
lovleenkaur <lovleenk99@gmail.com>
2023-06-30 21:06:56 -0400

    modified index.html, script.js, style.css, db.json

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+fetch("http://localhost:3001/restaurants")
+.then(response => response.json())
+//.then(restaurants => constructRestaurantElements(restaurants))
+.then(restaurants => { 
+  restaurants.forEach(restaurant => { 
+      renderRestaurants(restaurant)
+  })
+  showRestaurantDetail(restaurants[0])
+})
 
 
-//const titleBesu = document.querySelector('#title-besu')
+function renderRestaurants(restaurants){ 
+    
+  const nav = document.querySelector('#restaurant-display')
+  const img = document.createElement('img')
+
+  img.src = restaurants.image
+  nav.appendChild(img)
+
+  img.addEventListener('click', () => { 
+      showRestaurantDetail(restaurants)
+  })
+
+  img.addEventListener("mouseover", restaurants);
+    img.src = restaurants.image
+ 
+  
+}
+
+function showRestaurantDetail(list) { 
+  
+  const image = document.querySelector('.detail-image')
+  image.src = list.image
+
+  const name = document.querySelector('.name')
+  name.textContent = list.title
+
+  const description = document.querySelector('.description')
+  description.textContent = list.description
+
+  const comment = document.querySelector('#restaurant-comment')
+  comment.textContent = list.comment
+
+}
+
+const form = document.querySelector('#restaurant-comment')
+form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  
+  const newComment = e.target['new-comment'].value 
+  const newRestaurantComment = { 
+      
+      "comment" : newComment
+  }
+  renderRestaurants(newRestaurantComment)
+})
+
+
+
